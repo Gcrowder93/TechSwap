5aa3fb4be43469b5873cdb535e866a6b52db70c2
James Demiraiakian <james.armen@gmail.com>
2022-01-14 16:06:05 -0800

    got Auth Form rendering

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import react from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import AuthForm from './components/Auth/AuthForm';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <AuthForm />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
