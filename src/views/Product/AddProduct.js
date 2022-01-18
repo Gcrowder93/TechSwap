a3846caa196ced87e9ad5919cd22b59eedd1b4da
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-01-18 10:40:07 -0800

    created addProduct creating ProductForm to render in addProduct

--- src/views/Product/AddProduct.js
+++ src/views/Product/AddProduct.js
@@ -1,6 +1,46 @@
 import React from 'react';
+import { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { createProduct } from '../../services/products';
 
 export default function AddProduct() {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState('');
+  const [image, setImage] = useState('');
+  const [catagories, setCatagories] = useState('');
+  const [condition, setCondition] = useState('');
+  const [alert, setAlert] = useState('');
+  const history = useHistory();
+
+  const onChange = ({ target }) => {
+    switch (target.title) {
+      case 'title':
+        return setTitle(target.value);
+      case 'description':
+        return setDescription(target.value);
+      case 'price':
+        return setPrice(target.price);
+      case 'image':
+        return setImage(target.image);
+      case 'catagories':
+        return setCatagories(target.catagories);
+      case 'condition':
+        return setCondition(target.condition);
+    }
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const resp = createProduct({ title, description, price, image, catagories, condition });
+      history.push(`/procucts/${resp[0].id}`);
+    } catch (error) {
+      setAlert(error.message);
+    }
+  };
+
   return (
     <div>
       <h1>Add Product page</h1>
