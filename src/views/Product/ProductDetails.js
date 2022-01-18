ccafaa76948a1f4a012acf65eb2da8bfe83a722f
Brett Seifried <seifriedbrett@gmail.com>
2022-01-18 15:22:05 -0800

    cleaned up Product Details Page

--- src/views/Product/ProductDetails.js
+++ src/views/Product/ProductDetails.js
@@ -1,28 +1,41 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import ProductDetailCard from '../../components/Products/ProductDetailCard';
 import { getProductById } from '../../services/products';
 
 export default function ProductDetails() {
-  const { id } = useParams();
+  // const { id } = useParams();
+  const params = useParams();
   const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      const data = await getProductById(id);
+      const data = await getProductById(params.id);
       setProduct(data);
+      setLoading(false);
     };
     // getProductById(id).then(({ data }) => setProduct(data));
     fetchData();
-  }, [id]);
+  }, [params.id]);
+  if (loading) {
+    return <h1>loading</h1>;
+  }
 
   return (
     <div className="product-detail">
-      <h1>{product.title}</h1>
-      <img src={product.image} />
-      <h3>{product.price}</h3>
-      <h3>{product.condition}</h3>
-      <p>{product.description}</p>
+      <div key={product.id}>
+        <ProductDetailCard product={product} />
+      </div>
     </div>
   );
+}
+
+{
+  /* <h1>{product.title}</h1>
+<img src={product.image} />
+<h3>{product.price}</h3>
+<h3>{product.condition}</h3>
+<p>{product.description}</p> */
 }
