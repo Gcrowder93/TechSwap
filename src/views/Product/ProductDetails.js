e8a0a3f2a784219862215fbb56a44a817a71f6f5
Brett Seifried <seifriedbrett@gmail.com>
2022-01-18 14:54:40 -0800

    ProductDetails.js rendering products by id

--- src/views/Product/ProductDetails.js
+++ src/views/Product/ProductDetails.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getProductById } from '../../services/products';
 
 export default function ProductDetails() {
+  const { id } = useParams();
+  const [product, setProduct] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getProductById(id);
+      setProduct(data);
+    };
+    // getProductById(id).then(({ data }) => setProduct(data));
+    fetchData();
+  }, [id]);
+
   return (
-    <div>
-      <h1> Product Detail Page</h1>
-      <p>Picture, descript, contact, edit, delete</p>
+    <div className="product-detail">
+      <h1>{product.title}</h1>
+      <img src={product.image} />
+      <h3>{product.price}</h3>
+      <h3>{product.condition}</h3>
+      <p>{product.description}</p>
     </div>
   );
 }
