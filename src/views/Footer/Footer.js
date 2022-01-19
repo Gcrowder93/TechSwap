63cbd2a6ae530e3156d0d3bd200ec15087d4c159
Chase Crowder <gcrowder93@gmail.com>
2022-01-19 10:29:20 -0800

    fixed linkedin links

--- src/views/Footer/Footer.js
+++ src/views/Footer/Footer.js
@@ -44,7 +44,7 @@ const Footer = () => {
           </Column>
           <Column>
             <Heading>
-              <FooterLink href="/UserView">
+              <FooterLink href="/profile/:id">
                 <img
                   src="https://icon-library.com/images/profile-icon-white/profile-icon-white-1.jpg"
                   className="profilepic"
