ad04369d5300e701949f5c18b60f9dca02e361ea
Mark Voltaire <markjvoltaire@gmail.com>
2022-01-19 14:18:44 -0500

    added more css to text and images

--- src/components/Products/ProductsCard.js
+++ src/components/Products/ProductsCard.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 export default function ProductsCard({ products }) {
   return (
     <div>
-      <h1>Products Card</h1>
+      <h1 className="productListTitle">Welcome To The Alchemy TechSwap</h1>
 
       {products.map((item) => (
-        <div key={item.id}>
+        <div className="productLists" key={item.id}>
           <Link className="productLinks" to={`/products/${item.id}`}>
-            <h1>{item.title}</h1>
-            <p>{item.condition}</p>
-            <p>${item.price}</p>
-            <img className="productImages" src={item.image} />
+            <img className="productImage" src={item.image} />
+            <h1 className="productTitle">{item.title}</h1>
+            <p className="productCondition">{item.condition}</p>
+            <p className="productPrice">${item.price}</p>
           </Link>
         </div>
       ))}
