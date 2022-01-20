af3d6f063e655e279df2e0a7b1b48d2b95373b6a
Mark Voltaire <markjvoltaire@gmail.com>
2022-01-19 20:35:38 -0500

    button added but function not added

--- src/views/Product/EditProduct.js
+++ src/views/Product/EditProduct.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import { getProductById, updateProductById } from '../../services/products';
+import { getProductById, updateProductById, deleteProduct } from '../../services/products';
 import EditProductComp from '../../components/Products/EditProductComp';
 
 export default function EditProduct() {
