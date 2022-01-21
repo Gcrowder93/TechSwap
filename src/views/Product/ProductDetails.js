74742b42036a2dd55fa44eb092b2a80623c5a572
Brett Seifried <seifriedbrett@gmail.com>
2022-01-20 16:33:23 -0800

    updated readme.md

--- src/views/Product/ProductDetails.js
+++ src/views/Product/ProductDetails.js
@@ -3,17 +3,23 @@ import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import ProductDetailCard from '../../components/Products/ProductDetailCard';
 import { getProductById } from '../../services/products';
+import { getUserById } from '../../services/users';
+import { client } from '../../services/client';
 
 export default function ProductDetails() {
   // const { id } = useParams();
   const params = useParams();
   const [product, setProduct] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentUser, setCurrentUser] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await getProductById(params.id);
+      const user = await getUserById(client.auth.session().user.id);
       setProduct(data);
+      setCurrentUser(user);
+      console.log(user);
       setLoading(false);
     };
     // getProductById(id).then(({ data }) => setProduct(data));
@@ -23,10 +29,16 @@ export default function ProductDetails() {
     return <h1>loading</h1>;
   }
 
+  const onClick = async () => {
+    return window.open(
+      `mailto:${currentUser.email}?subject=I'm interested in your ${product.title}`
+    );
+  };
+
   return (
     <div className="product-detail">
       <div key={product.id}>
-        <ProductDetailCard product={product} />
+        <ProductDetailCard product={product} currentUser={currentUser} onClick={onClick} />
       </div>
     </div>
   );
