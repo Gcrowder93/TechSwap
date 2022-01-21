e83d1665d917360f3508f1c9f39dbf12211ad41e
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-01-20 18:24:58 -0800

    fixed authForm test

--- src/components/Auth/AuthForm.test.js
+++ src/components/Auth/AuthForm.test.js
@@ -3,16 +3,20 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 test('Should render Auth Form', async () => {
-  <MemoryRouter>
-    <AuthForm
-      user={{
-        email: '',
-        errorMessage: '',
-        handleSubmit: 'ƒ handleSubmit() {}',
-        password: '',
-        setEmail: 'ƒ bound dispatchAction() {}',
-        setPassword: 'ƒ bound dispatchAction() {}',
-      }}
-    />
-  </MemoryRouter>;
+  const { container } = render(
+    <MemoryRouter>
+      <AuthForm
+        user={{
+          email: '',
+          errorMessage: '',
+          handleSubmit: 'ƒ handleSubmit() {}',
+          password: '',
+          setEmail: 'ƒ bound dispatchAction() {}',
+          setPassword: 'ƒ bound dispatchAction() {}',
+        }}
+      />
+    </MemoryRouter>
+  );
+  await screen.findByText('Email:');
+  expect(container).toMatchSnapshot();
 });
